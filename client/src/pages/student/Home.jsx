f1e4cdd5effd72fbb7b9fa35798058a74b0a459e
manaswinim2004 <143214915+manaswinim2004@users.noreply.github.com>
2025-08-18 04:54:28 +0530

    new

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Hero from '../../components/student/Hero'
-import Companies from '../../components/student/Companies'
-import CoursesSection from '../../components/student/CoursesSection'
-import TestimonialsSection from '../../components/student/TestimonialsSection'
-import CallToAction from "../../components/student/CallToAction";
+import Hero from '../../components/student/Hero.jsx'
+import Companies from '../../components/student/Companies.jsx'
+import CoursesSection from '../../components/student/CoursesSection.jsx'
+import TestimonialsSection from '../../components/student/TestimonialsSection.jsx'
+import CallToAction from "../../components/student/CallToAction.jsx";
 
-import Footer from '../../components/student/footer'
+import Footer from '../components/student/Footer.jsx'
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
